cbc5f3d73866c16c9503ddef17d96d36292eafa8
Vu Xuan Hoai <86643318+Hoaivuxuan@users.noreply.github.com>
2025-05-21 15:38:13 +0700

    update data

--- src/data.js
+++ src/data.js
@@ -3,19 +3,28 @@
 export const projects = [
   {
     title: "Enterprise resource planning erp(CRM, HCM)",
-    subtitle: "Frontend Developer",
+    subtitle: "",
     description:
       "From human resource management, production line and material supply management, internal financial management to sales, product marketing, communication with partners, customers... are all fully integrated in this product of ours.",
     image: "https://www.easternsun.vn/wp-content/uploads/2022/06/erp.png",
     link: "https://docs.google.com/document/d/1HJn4N30-bbvFuS7A1EWaeumi9iMUCZa5aQcZ4VxuBXw/edit?tab=t.0",
   },
   {
-    title: "CUSTOMER RELATIONSHIP MANAGEMENT SOFTWARE",
-    subtitle: "Frontend Developer",
+    title: "Customer Relationship Management Software",
+    subtitle: "",
     description:
       "Customer relationship management software is a comprehensive solution designed to support businesses in customer management, optimizing customer care processes, increasing customer experience and building customer loyalty.",
     image: "https://base.vn/wp-content/uploads/2024/09/crm-la-gi.webp",
     link: "https://github.com/Hoaivuxuan/crm-system",
+  },
+  {
+    title: "Weather Forecast Application",
+    subtitle: "",
+    description:
+      "Weather App is a mobile application built using React Native and Expo that provides users with real-time weather information and forecasts. The app fetches weather data from multiple APIs including OpenWeatherMap and WeatherAPI to display current weather conditions, hourly forecasts, and 7-day weather predictions. Features include current weather display, temperature charts, precipitation forecasts, UV index information, and moon phase details. Users can search for weather in different locations and use their current location for local weather updates.",
+    image:
+      "https://media.daily.dev/image/upload/f_auto,q_auto/v1/posts/3c40b279c39400fb2849655f0228807b?_a=AQADKdt",
+    link: "https://github.com/Hoaivuxuan/weather-app-v2",
   },
 ];
 
